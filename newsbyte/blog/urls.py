e6476a9101f47f74ba117d0b8825caf6f5babbe3
struk49 <andrewstruk48@gmail.com>
2025-05-07 17:13:23 +0000

    added post detal view

--- newsbyte/blog/urls.py
+++ newsbyte/blog/urls.py
@@ -1,10 +1,12 @@
 # blog/urls.py
 from django.urls import path, include
-from blog.views import IndexView  # Import IndexView from views.py
+from blog.views import IndexView, DetailArticleView # Import IndexView from views.py
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),  # Homepage URL points to IndexView
     path('tinymce/', include('tinymce.urls')),
     # Other blog-specific URLs, e.g.:
     # path('post/<int:id>/', PostDetailView.as_view(), name='post_detail'),
+    path('article/<int:pk>/', DetailArticleView.as_view(), name='article-detail'),
+
 ]
--- newsbyte/blog/views.py
+++ newsbyte/blog/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Article
 
@@ -9,3 +11,12 @@ class IndexView(LoginRequiredMixin, ListView):
     paginate_by = 1
     ordering = ['-date']
     login_url = 'login'
+
+
+class DetailArticleView(LoginRequiredMixin, DetailView):
+    model = Article
+    template_name = 'blog/blog_post.html'
+    context_object_name = 'article'
+    login_url = 'login'
+
+
